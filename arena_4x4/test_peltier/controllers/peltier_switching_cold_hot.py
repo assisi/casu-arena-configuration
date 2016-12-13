ffb24b1839d054ab89882de72cdccd6cc0f05260
Damjan Miklic <damjan.miklic@gmail.com>
2016-12-13 19:29:23 +0100

    - removed old arena configs because they are invalid in the current setup - reorganized the arena_4x4 folder layout to accomodate different testing scenarios - added a scenario for testing peltier heating/cooling

---
+++ arena_4x4/test_peltier/controllers/peltier_switching_cold_hot.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+
+from assisipy import casu
+
+if __name__ == '__main__':
+
+    c = casu.Casu(sys.argv[1])
+
+    time_begin = time.time()
+    time_now = time.time()
+
+    while(time_now - time_begin < 1800):
+
+        c.set_temp(30)
+        c.set_diagnostic_led_rgb(b=1)
+        time.sleep(300)
+
+        c.set_temp(35)
+        c.set_diagnostic_led_rgb(r=1)
+        time.sleep(300)
+        
+        time_now = time.time()
